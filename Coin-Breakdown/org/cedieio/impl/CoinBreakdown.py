97587c07284e303fcc198196ba70d12d152eaaba
cedieio <cedric@Cedric-PC.localdomain>
2019-10-05 18:50:28 +0800

    move to folders

---
+++ Coin-Breakdown/org/cedieio/impl/CoinBreakdown.py
@@ -0,0 +1,22 @@
+# Greedy algorithm
+
+def coin_breakdown(amount):
+    arr_val = []
+
+    coins = [25, 10, 5, 1]
+    for coin in coins:
+        if amount >= coin:
+            num_of_appearance = amount // coin
+            arr_val.append("Coin {0}, number of times {1}".format(coin, num_of_appearance) )
+            amount = amount - (num_of_appearance * coin)
+    return arr_val
+
+
+print (coin_breakdown(1))
+print (coin_breakdown(26))
+print (coin_breakdown(33))
+print (coin_breakdown(38))
+
+
+def coin_breakdown_with_override(amount, coin_count):
+    for 
